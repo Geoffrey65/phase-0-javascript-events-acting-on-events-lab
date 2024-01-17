6dca5d7f7c0d09cffd682d06432f5c75f7216473
Geoffrey <geoffrey>
2024-01-17 16:47:44 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+function isPalindrome(word) {
+    // Convert the word to lowercase to handle case-insensitivity.
+    const lowercaseWord = word.toLowerCase();
+  
+    // Removing  non-letter characters using regex.
+    const cleanWord = lowercaseWord.replace(/[^a-z]/g, '');
+  
+    // Reversing the cleaned word.
+    const reversedWord = cleanWord.split('').reverse().join('');
+  
+    // Checking if the reversed word is equal to the original word.
+    return cleanWord === reversedWord;
+  }
+  
+  // Custom Tests:
+  if (require.main === module) {
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar")); // Should return true
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot")); // Should return false
+  }
+  
+  module.exports = isPalindrome;
+  
